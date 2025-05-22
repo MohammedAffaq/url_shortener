300a778fa940c2da12725860ad3ec45f748f9c85
Mohammed Affaq <mohammedaffaq1431@gmail.com>
2025-05-23 02:22:53 +0530

    second commit

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -5,7 +5,7 @@ export default async function Page({ params }) {
     const { shorturl } = await params
 
     const client = await clientPromise;
-    const db = client.db("bitlinks")
+    const db = client.db("BitLinks")
     const collection = db.collection("url")
 
     const doc = await collection.findOne({shorturl: shorturl})
